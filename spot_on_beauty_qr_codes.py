aa62254670da73850f269f2090fa0093bb150cc2
Daniel Ratonyi <dani@ratonyi.com>
2024-12-12 17:35:18 -0500

    saving progress

--- spot_on_beauty_qr_codes.py
+++ spot_on_beauty_qr_codes.py
@@ -8,21 +8,22 @@ def draw_ui(canvas):
 
     # Load and display the logo image on the canvas
     logo = Image.open("resources/spot_on_beauty.png")  # Load the image
-    logo_resized = logo.resize((200, 100))  # Resize if needed to fit canvas dimensions
+    logo_resized = logo.resize((400, 133))  # Resize if needed to fit canvas dimensions
     logo_image = ImageTk.PhotoImage(logo_resized)  # Convert to PhotoImage for Tkinter
 
     # Keep a reference to avoid garbage collection
     canvas.image = logo_image
 
     # Draw the image on the canvas
-    canvas.create_image(110, 110, image=logo_image)  # Centered in the 220x220 canvas
+    canvas.create_image(250, 110, image=logo_image)  # Centered in the 220x220 canvas
 
 def update_ui():
     try:
         link = entry.get()
-        
-        if not link:
-            messagebox.showerror("Error", "Please enter text for the QR Code.")
+        file_name = file_name_entry.get()
+
+        if not link or not file_name:
+            messagebox.showerror("Error", "Please enter a name for the QR Code file and a link")
             return
 
         # Generate a QR code
@@ -32,8 +33,8 @@ def update_ui():
         qr_image = qr.make_image()
 
         # Save the QR code image
-        qr_image.save("qr_code_test.png")
-        messagebox.showinfo("Success", "QR Code has been generated and saved as 'qr_code_test.png'.")
+        qr_image.save("qrCodes/" + file_name + ".png")
+        messagebox.showinfo("Success", "QR Code has been generated and saved in the qrCodes folder")
 
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {e}")
@@ -45,23 +46,30 @@ def update_ui():
 root = tk.Tk()
 root.title("QR Code Generator")
 
+# Canvas for drawing the gauge
+canvas = Canvas(root, width=500, height=200, bg="white")
+canvas.pack()
+
+# Initialize the gauge to a default value and display the logo
+draw_ui(canvas)
+
 # Label for the Entry
-entry_label = tk.Label(root, text="Enter text for QR Code:")
+file_name_label = tk.Label(root, text="Enter the name for the QR Code:")
+file_name_label.pack()
+
+file_name_entry = tk.Entry(root, width=25)
+file_name_entry.pack()
+
+# Label for the Entry
+entry_label = tk.Label(root, text="Enter the link for QR Code:")
 entry_label.pack()
 
 # Entry widget for text input
-entry = tk.Entry(root)
+entry = tk.Entry(root, width=50)
 entry.pack()
 
 # Button to generate QR Code
 update_button = tk.Button(root, text="Generate QR Code", command=update_ui)
 update_button.pack()
-
-# Canvas for drawing the gauge
-canvas = Canvas(root, width=400, height=200, bg="white")
-canvas.pack()
-
-# Initialize the gauge to a default value and display the logo
-draw_ui(canvas)
 
 root.mainloop()
